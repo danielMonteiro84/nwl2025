f822983b04ab05b60c15b1d1317a25de31085911
daniel <danielchsan@gmail.com>
2025-02-21 17:47:19 -0300

    estilizando `page invite`

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import logo from "../../assets/logo.svg";
+import gold from "../../assets/medal-gold.svg";
+import silver from "../../assets/medal-silver.svg";
+import cooper from "../../assets/medal-cooper.svg";
 import { InputField, InputIcon, InputRoot } from "@/components/input";
-import { Copy, Link } from "lucide-react";
+import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from "lucide-react";
 import { IconButton } from "@/components/icon-button";
 
 export default function InvitePage() {
@@ -41,32 +44,72 @@ export default function InvitePage() {
             </IconButton>
           </InputRoot>
           <div className="grid gap-3 md:grid-cols-3">
-            <div className="bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rouded-xl">
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rouded-xl">
               <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
                 1042
               </span>
               <span className="text-sm text-gray-300 leading-none tet-center">
                 Acessos ao link
               </span>
+              <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
             </div>
 
-            <div className="bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rouded-xl">
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rouded-xl">
               <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
                 785
               </span>
               <span className="text-sm text-gray-300 leading-none tet-center">
                 Inscrições Feitas
               </span>
+              <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
             </div>
 
-            <div className="bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rouded-xl">
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rouded-xl">
               <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
                 3º
               </span>
               <span className="text-sm text-gray-300 leading-none tet-center">
                 Posição no ranking
               </span>
+              <Medal className="size-5 text-purple absolute top-3 left-3" />
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[400px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Ranking de indicações
+        </h2>
+        <div className="space-y-4">
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1º</span> | Daniel Monteiro
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image src={gold} alt="" className="absolute top-0 right-8" />
+          </div>
+
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">2º</span> | Daniel Monteiro
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image src={silver} alt="" className="absolute top-0 right-8" />
+          </div>
+
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">3º</span> | Daniel Monteiro
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image src={cooper} alt="" className="absolute top-0 right-8" />
           </div>
         </div>
       </div>
